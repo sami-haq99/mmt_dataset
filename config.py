bf8d86c08f2ab8c61e3d40e4c601672c3534c106
sami-haq99 <sami.haq99@gmail.com>
2026-02-19 14:11:46 +0000

    s

--- config.py
+++ config.py
@@ -7,12 +7,13 @@ EMBEDDING_FILE = "image_embeddings.memmap"
 
 EMBEDDING_DIM = 2048
 BATCH_SIZE = 64
-NUM_WORKERS = 8         # PyTorch DataLoader workers
+NUM_WORKERS = 16         # PyTorch DataLoader workers
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # IVF-PQ params for large scale
-N_LIST = 2# 8192
-M = 4 # 64 # number of subquantizers
-N_BITS = 8
-TOP_K = 10
+N_LIST = 1000   # 1000 coarse clusters
+M = 64          # 32-dim PQ sub-vectors
+N_BITS = 8      # 1 byte per subvector
+
+TOP_K = 5
 INDEX_FILE = "ivfpq_index_1M.index"
