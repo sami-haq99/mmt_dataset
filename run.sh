67b7d0a5c354ee9158620a21148c54636e8938f3
sami-haq99 <sami.haq99@gmail.com>
2026-02-19 14:14:25 +0000

    s

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#SBATCH --gres=gpu:a100:3
+#SBATCH -p compute
+#SBATCH -J mm_ss
+#SBATCH -t 23:59:59
+#SBATCH -o jina-cluster-%j.out
+#SBATCH --mail-type=ALL --mail-user=sami.haq@adaptcentre.ie
+
+
+# For ADAPT Cluster
+
+source /home/shaq/mtqe/env-mtqe/bin/activate
+
+
+python embed_images_multigpu.py
+
+echo "Embedding complete. Building FAISS index..."
+
+python build_faiss_index.py
+
